844d543ed6286ba647d667fd912771cc2787acde
T-K-T-K-T <k24128kk@aitech.ac.jp>
2025-12-18 15:10:30 +0900

    あ

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
 from flask import Flask, render_template
 from models import initialize_database
 from routes import blueprints
+from models.product import Product
+from models.order import Order
+from peewee import fn
 
 app = Flask(__name__)
 
-# データベースの初期化
 initialize_database()
 
-# 各Blueprintをアプリケーションに登録
 for blueprint in blueprints:
     app.register_blueprint(blueprint)
 
-# ホームページのルート
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # 映画ごとの注文数（＝見た人数）を集計
+    query = (
+        Product
+        .select(Product.name, fn.COUNT(Order.id).alias('count'))
+        .join(Order)
+        .group_by(Product.name)
+    )
+
+    labels = []
+    data = []
+
+    for row in query:
+        labels.append(row.name)
+        data.append(row.count)
+
+    return render_template(
+        'index.html',
+        labels=labels,
+        data=data
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
